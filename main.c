50f847b9ff32c919922a82823da7ff31409acc5d
John Long <johnzl@protonmail.ch>
2019-03-01 10:17:03 -0800

    Added comments

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/*
+ * 1.) Assume Data Read
+ * If sensor has pair, check if data values are 5% within. Send back status that both sensors are nominal
+ * If not, check SD of past 10 samples. If sensor is greater than 3SD away from mean, then failure is detected
+ *
+ */
+
+
 int sensorstate(double,double);
 void obtain_value(double);
 
@@ -11,17 +19,17 @@ double arr[];
 int i;
 double standardDeviation;
 
-//Distance Laser Sensors
+//Distance Laser Sensors (PAIR)
 double C_Las0,C_Las1;
-//Distance Lidar Sesnors
+//Distance Lidar Sesnors (PAIR)
 double SX_Las0,SX_Las1;
-//Pressure (Braking) Sensors
+//Pressure (Braking) Sensors (NON-PAIR)
 double B_Pres0,B_Pres1,B_Pres2,B_Pres3,B_Pres4,B_Pres5,B_Pres6,B_Pres7,B_Pres8,B_Pres9;
-//Thermocouple Probes for Braking
+//Thermocouple Probes for Braking (PAIR)
 double B_Tem0,B_Tem1;
-//Pressure (Propulsion) Sensors
+//Pressure (Propulsion) Sensors (NON-PAIR)
 double P_Pres0,P_Pres1,P_Pres2,P_Pres3,P_Pres4;
-//Propulsion Pressure Sensors
+//Propulsion Pressure Sensors (NON-PAIR)
 double P_Tem0,P_Tem1,P_Tem2,P_Tem3,P_Tem4,P_Tem5,P_Tem6,P_Tem7,P_Tem8;
 
 int main()
